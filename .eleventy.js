e69f73693ff30bb30b9e1292018a3ec0fb8fb7e9
chwmn <84538816+chwmn@users.noreply.github.com>
2023-05-05 17:38:20 -0500

    more setup

---
+++ .eleventy.js
@@ -0,0 +1,22 @@
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addWatchTarget("./src/css/");
+  eleventyConfig.setTemplateFormats(["css", "html", "liquid"]);
+  eleventyConfig.addPassthroughCopy("scripts");
+  eleventyConfig.addPassthroughCopy("assets");
+
+  function sortByPageOrder(values) {
+    return values.slice().sort((a, b) => a.data.order - b.data.order);
+  }
+
+  eleventyConfig.addFilter("sortByPageOrder", sortByPageOrder);
+
+  // You can return your Config object (optional).
+  return {
+    dir: {
+      includes: "_includes",
+      layouts: "_layouts",
+      input: "src",
+      output: "dist",
+    },
+  };
+};
